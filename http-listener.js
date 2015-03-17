10e195b6f6b83453d0d94de647cbc4abd6c09917
Alex Graff <alex.graff@7digital.com>
2015-03-17 13:37:46 +0000

    Return 'Hello world' in response.

--- http-listener.js
+++ http-listener.js
@@ -4,14 +4,14 @@ var port=process.argv[2] || 8888;
 var address=process.argv[3] || "127.0.0.1";
 
 var server = http.createServer(function (request, response) {
-  console.log("REQUEST");
+  console.log("REQUEST RECEIVED");
   console.log("   url: " + request.method + " " + request.url);
   request.on("data", function(chunk) {
     console.log("   body: " + chunk);
   });
 
-  response.writeHead(200);
-  response.end();
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.end("Hello world!");
 });
 
 server.listen(port, address);
